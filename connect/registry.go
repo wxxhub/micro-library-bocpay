f4ba5eed2ccb70f8aa9bd574bc07d1e6a59a4655
wangxu <wangxu@oneniceapp.com>
2020-01-22 14:07:33 +0800

    添加检查间隔为60s

--- connect/registry.go
+++ connect/registry.go
@@ -4,10 +4,12 @@ import (
 	"github.com/micro/go-micro/registry"
 	consulRegistry "github.com/micro/go-plugins/registry/consul"
 	"os"
+	"time"
 )
 
 func NewConsulRegistry() registry.Registry {
 	return consulRegistry.NewRegistry(
 		registry.Addrs(os.Getenv("CONSUL_ADDR")),
+		consulRegistry.TCPCheck(time.Second*60),
 	)
 }
