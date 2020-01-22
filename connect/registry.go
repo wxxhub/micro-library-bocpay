acfa76025ddab4d6ec3f1ec02906fe878a4ca1e6
wangxu <wangxu@oneniceapp.com>
2020-01-22 15:31:27 +0800

    删除上次修改

--- connect/registry.go
+++ connect/registry.go
@@ -4,12 +4,10 @@ import (
 	"github.com/micro/go-micro/registry"
 	consulRegistry "github.com/micro/go-plugins/registry/consul"
 	"os"
-	"time"
 )
 
 func NewConsulRegistry() registry.Registry {
 	return consulRegistry.NewRegistry(
 		registry.Addrs(os.Getenv("CONSUL_ADDR")),
-		consulRegistry.TCPCheck(time.Second*60),
 	)
 }
