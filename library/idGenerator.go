fb65ec798bd9771743110919048d1e1645aeb58b
lifenglin <lifenglin@oneniceapp.com>
2020-05-08 18:08:04 +0800

    update

--- library/idGenerator.go
+++ library/idGenerator.go
@@ -13,19 +13,24 @@ func GetIncrementId(ctx context.Context, hlp *helper.Helper) (id uint64, err err
 		return 0, nil
 	}
 	//重试2次
-	for i := 0; i < 2; i++ {
+	retry := 2
+	i := 0
+	for {
 		id, err = redis.Do("getid").Uint64()
 		if err == nil {
 			return id, nil
-		}
-		pong, err := redis.Ping().Result()
-		if err != nil {
-			hlp.RedisLog.WithFields(logrus.Fields{
-				"pong":  pong,
-				"error": err.Error(),
-			}).Error("connect redis fail")
+		} else {
+			pong, err := redis.Ping().Result()
+			if i <= retry {
+				i++
+				continue
+			} else {
+				hlp.RedisLog.WithFields(logrus.Fields{
+					"pong":  pong,
+					"error": err.Error(),
+				}).Error("connect redis fail")
+				return 0, err
+			}
 		}
 	}
-	return 0, err
-
 }
