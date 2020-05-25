f53b4a3f50887a7bc19bf5ee228e8c6ad2bf43af
刘佐成 <liuzuocheng@oneniceapp.com>
2020-05-25 16:47:14 +0800

    rand seed

--- library/string.go
+++ library/string.go
@@ -4,11 +4,13 @@ import (
 	"crypto/md5"
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func GetRandStr(n int) string {
 	seedsLetters := []byte("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 	e := make([]byte, n)
+	rand.Seed(time.Now().UnixNano())
 	for i := 0; i < n; i++ {
 		e[i] = seedsLetters[rand.Intn(len(seedsLetters))]
 	}
