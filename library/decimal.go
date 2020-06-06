f259314fee115968a2d5d06509c5a5c547b6c5df
lifenglin <lifenglin@oneniceapp.com>
2020-06-06 16:15:18 +0800

    update

--- library/decimal.go
+++ library/decimal.go
@@ -4,12 +4,12 @@ import (
 	"github.com/shopspring/decimal"
 )
 
-func Yuan2penny(from int64) string {
+func Penny2Yuan(from int64) string {
 	d := decimal.New(1,2)
 	return decimal.NewFromInt(from).DivRound(d,2).String()
 }
 
-func Penny2Yuan(from string) int64 {
+func Yuan2penny(from string) int64 {
 	d := decimal.New(1,2)//分转元乘以100
 	d1 := decimal.New(1,0)//乘完之后，保留2为小数，需要这么一个中间参数
 	//当乘以100后，仍然有小数位，取四舍五入法后，再取整数部分
