bcaa46b47f8c62878e8dcd3c6f6b5598c2dd94a3
lifenglin <lifenglin@oneniceapp.com>
2020-06-17 12:11:09 +0800

    update

--- connect/alipay.go
+++ connect/alipay.go
@@ -26,9 +26,26 @@ func ConnectAlipay(srvName string, confName string) (*alipay.Client, error) {
 		return nil, errors.InternalServerError(srvName, "privateKey is empty")
 	}
 
+	/*
 	publicKey := conf.Get(srvName, confName, "publicKey").String("")
 	if privateKey == "" {
 		return nil, errors.InternalServerError(srvName, "publicKey is empty")
+	}
+	*/
+
+	certPublicKeyRSA2 := conf.Get(srvName, confName, "certPublicKeyRSA2").String("")
+	if certPublicKeyRSA2 == "" {
+		return nil, errors.InternalServerError(srvName, "certPublicKeyRSA2 is empty")
+	}
+
+	rootCert := conf.Get(srvName, confName, "rootCert").String("")
+	if certPublicKeyRSA2 == "" {
+		return nil, errors.InternalServerError(srvName, "certPublicKeyRSA2 is empty")
+	}
+
+	certPublicKey := conf.Get(srvName, confName, "certPublicKey").String("")
+	if certPublicKeyRSA2 == "" {
+		return nil, errors.InternalServerError(srvName, "certPublicKey is empty")
 	}
 
 	isProduction := conf.Get(srvName, confName, "isProduction").Bool(false)
@@ -38,9 +55,24 @@ func ConnectAlipay(srvName string, confName string) (*alipay.Client, error) {
 		return nil, errors.InternalServerError(srvName, "new alipay fail: %v", err.Error())
 	}
 
+	/*
 	err = alipayClient.LoadAliPayPublicKey(publicKey)
 	if err != nil {
 		return nil, errors.InternalServerError(srvName, "load public key fail: %v", err.Error())
+	}
+	*/
+
+	err = alipayClient.LoadAppPublicCert(certPublicKey) // 加载应用公钥证书
+	if err != nil {
+		return nil, errors.InternalServerError(srvName, "LoadAppPublicCert fail: %v", err.Error())
+	}
+	err = alipayClient.LoadAliPayRootCert(rootCert) // 加载支付宝根证书
+	if err != nil {
+		return nil, errors.InternalServerError(srvName, "LoadAliPayRootCert fail: %v", err.Error())
+	}
+	err = alipayClient.LoadAliPayPublicCert(certPublicKeyRSA2) // 加载支付宝公钥证书
+	if err != nil {
+		return nil, errors.InternalServerError(srvName, "LoadAliPayPublicCert fail: %v", err.Error())
 	}
 
 	return alipayClient, nil
