e7a5e4c44b923d21b6036c9a99bc0c264d7df150
wangxu <wangxu@oneniceapp.com>
2020-08-26 19:38:10 +0800

    去掉zipkin

--- connect/jaeger.go
+++ connect/jaeger.go
@@ -3,18 +3,11 @@ package connect
 import (
 	"github.com/opentracing/opentracing-go"
 	"github.com/uber/jaeger-client-go"
-	"github.com/uber/jaeger-client-go/zipkin"
 	"log"
 	"time"
 )
 
 func InitJaeger(srvName string) {
-	zipkinPropagator := zipkin.NewZipkinB3HTTPHeaderPropagator()
-	injector := jaeger.TracerOptions.Injector(opentracing.HTTPHeaders, zipkinPropagator)
-	extractor := jaeger.TracerOptions.Extractor(opentracing.HTTPHeaders, zipkinPropagator)
-
-	zipkinSharedRPCSpan := jaeger.TracerOptions.ZipkinSharedRPCSpan(true)
-
 	sender, err := jaeger.NewUDPTransport("jaeger-agent.default:5775", 0)
 	if err != nil {
 		log.Println("connect jaeger err: ", err)
@@ -28,9 +21,6 @@ func InitJaeger(srvName string) {
 			sender,
 			jaeger.ReporterOptions.BufferFlushInterval(10*time.Second),
 		),
-		injector,
-		extractor,
-		zipkinSharedRPCSpan,
 	)
 
 	opentracing.SetGlobalTracer(tracer)
