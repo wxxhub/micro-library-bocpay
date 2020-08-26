e5c78a41573979b41366f2d2b9314c887e9683bf
wangxu <wangxu@oneniceapp.com>
2020-08-26 15:12:17 +0800

    重构jaeger

--- connect/init.go
+++ connect/init.go
@@ -1,25 +1,7 @@
 package connect
 
-import (
-	"github.com/opentracing/opentracing-go"
-	"log"
-)
-
-func Initialization(serviceName string) opentracing.Tracer {
-	err := ConnectLog(serviceName)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = ConnectStdLog(serviceName)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	tracer, err := ConnectJaeger(serviceName)
-	if err != nil {
-		log.Println("connect jaeger err: ", err)
-	}
-
-	return tracer
+func Initialization(serviceName string) {
+	_ = ConnectLog(serviceName)
+	_ = ConnectStdLog(serviceName)
+	InitJaeger(serviceName)
 }
--- connect/jaeger.go
+++ connect/jaeger.go
@@ -3,21 +3,31 @@ package connect
 import (
 	"github.com/opentracing/opentracing-go"
 	"github.com/uber/jaeger-client-go"
+	"log"
 	"time"
 )
 
-func ConnectJaeger(srvName string) (opentracing.Tracer, error) {
+func InitJaeger(srvName string) {
 	sender, err := jaeger.NewUDPTransport("jaeger-agent.default:5775", 0)
 	if err != nil {
-		return nil, err
+		log.Println("connect jaeger err: ", err)
+		return
+	}
+
+	sampler, err := jaeger.NewProbabilisticSampler(0.01)
+	if err != nil {
+		log.Println("sampler err: ", err)
+		return
 	}
 
 	tracer, _ := jaeger.NewTracer(
 		srvName,
-		jaeger.NewConstSampler(true),
+		sampler,
 		jaeger.NewRemoteReporter(
 			sender,
-			jaeger.ReporterOptions.BufferFlushInterval(1*time.Second)),
+			jaeger.ReporterOptions.BufferFlushInterval(10*time.Second),
+		),
 	)
-	return tracer, nil
+
+	opentracing.SetGlobalTracer(tracer)
 }
